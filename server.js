b1077275cef3c36c2522d65f333becbd88322560
Shivamcool400 <66465485+Shivamcool400@users.noreply.github.com>
2021-08-05 00:32:50 +0530

    server made

---
+++ server.js
@@ -0,0 +1,34 @@
+const firebase = require("firebase");
+// Required for side-effects
+require("firebase/firestore");
+
+// Initialize Cloud Firestore through Firebase
+firebase.initializeApp({
+    apiKey: "AIzaSyA_xrZBaYgCCIbxtpwWQAtB-imEUalRgiE",
+  authDomain: "bingo-a314a.firebaseapp.com",
+  projectId: "bingo-a314a",
+  storageBucket: "bingo-a314a.appspot.com",
+  messagingSenderId: "1072613042990",
+  appId: "1:1072613042990:web:4fb753084f7fdc7642e8bf",
+  measurementId: "G-3XY4FSC9MC"
+});
+  
+var db = firebase.firestore();
+
+
+db.collection("bingo").doc("lounge").set({name:"aryan"}).then(()=>{
+    console.log("Lounge was made");
+});
+
+db.collection("bingo").doc("lounge").get().then(doc => {
+    if (!doc.exists) {
+        console.log('doc not found');
+
+    }
+    {
+      console.log(doc.data());  
+    }
+}).catch(err => {
+    console.error("error getting document",err);
+})
+
